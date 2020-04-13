74722a7b7abb789ce6263f428d6740d432481407
meawoppl <meawoppl@gmail.com>
2020-04-13 09:29:35 -0700

    Comments/indentation

--- script.js
+++ script.js
@@ -23,7 +23,6 @@ doTheWork = function() {
         for (; i >= 0; i--,j++) {
             this.bgs[j] = {};
             this.bgs[j].cvs = document.getElementById(bgs[i].id);
-            console.log(this.bgs[j].cvs);
             this.bgs[j].ctx = this.bgs[j].cvs.getContext('2d');
             this.bgs[j].img = document.createElement('img');
             this.bgs[j].img.src = bgs[i].src;
@@ -34,48 +33,44 @@ doTheWork = function() {
         // According to scroll speed, render images in corresponding canvas
         // To have a seamless effect, render image twice
         scene: function() {
-        var i = this.bgs.length - 1,
-            left, w, h, bgNow, bgCtx;
-        for (; i >= 0; i--) {
-            bgNow = this.bgs[i];
-            left = bgNow.left;
-            w = bgNow.cvs.width;
-            h = bgNow.cvs.height;
-            bgCtx = bgNow.ctx;
-    
-            bgCtx.clearRect(0, 0, w, h);
-            bgCtx.drawImage(this.bgs[i].img, -left, 0, w, h);
-            bgCtx.drawImage(this.bgs[i].img, w - left, 0, w, h);
-            this.bgs[i].left = (left + this.bgs[i].speed) % w;
-        }
-    
+            var i = this.bgs.length - 1,
+                left, w, h, bgNow, bgCtx;
+            for (; i >= 0; i--) {
+                bgNow = this.bgs[i];
+                left = bgNow.left;
+                w = bgNow.cvs.width;
+                h = bgNow.cvs.height;
+                bgCtx = bgNow.ctx;
+        
+                bgCtx.clearRect(0, 0, w, h);
+                bgCtx.drawImage(this.bgs[i].img, -left, 0, w, h);
+                bgCtx.drawImage(this.bgs[i].img, w - left, 0, w, h);
+                this.bgs[i].left = (left + this.bgs[i].speed) % w;
+            }
         }
     };
     
-    // This handles looping . A wrapper around window.requestAnimationFrame
-    // Considers frame rate.
+    // This handles looping . A wrapper around `window.requestAnimationFrame`
     var Loop = {
         run: function(scene, fps) {
-        this.scene = scene;
-        this.frameInterval = 1000 / fps;
-        this.lastTime = new Date().getTime();
-    
-        this.loop();
-        },
+            this.scene = scene;
+            this.frameInterval = 1000 / fps;
+            this.lastTime = new Date().getTime();
+        
+            this.loop();
+            },
         loop: function() {
-        var now = new Date().getTime();
-        var elapsed = now - this.lastTime;
-        if (elapsed >= this.frameInterval) {
-    
-            this.scene();
-            this.lastTime = now;
-        }
-        window.requestAnimationFrame(this.loop.bind(this));
+            var now = new Date().getTime();
+            var elapsed = now - this.lastTime;
+            if (elapsed >= this.frameInterval) {
+                this.scene();
+                this.lastTime = now;
+            }
+            window.requestAnimationFrame(this.loop.bind(this));
         }
     };
     
-    // Initialize scenery
+    // Initialize scenery and run
     Scenery.init(bgs);
-    // Run it
     Loop.run(Scenery.scene.bind(Scenery), 60);
 }
